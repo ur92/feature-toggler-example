e4d4e60e63e8adb1841e08373ec4fe33710e39a4
Evgeni Dikerman <evgenid@jfrog.com>
2020-12-06 17:29:11 +0200

    initial

---
+++ src/components/GitBranch.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ greeting }}</h1>
+    <button>
+      <h3>Push to origin/{{ branch }}</h3>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GitBranch',
+  props: {
+    branch: {
+      type: String,
+      default: 'master'
+    }
+  },
+  data: () => ({
+    greeting: 'LIQUEFY YOUR FRONTEND VERSION'
+  })
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
